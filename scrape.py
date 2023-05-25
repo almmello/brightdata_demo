ac643aeada43de94a7a314d2339a7218c92ab39b
almmello <almmello@gmail.com>
2023-05-25 16:41:37 -0300

    running

--- scrape.py
+++ scrape.py
@@ -5,12 +5,14 @@ from playwright.async_api import async_playwright
 
 load_dotenv()  # take environment variables from .env.
 
+# should look like 'brd-customer-<ACCOUNT ID>-zone-<ZONE NAME>:<PASSWORD>'
 account_id = os.getenv('ACCOUNT_ID')
 zone_name = os.getenv('ZONE_NAME')
 password = os.getenv('PASSWORD')
+host = os.getenv('HOST')
 
 auth = f'brd-customer-{account_id}-zone-{zone_name}:{password}'
-browser_url = f'https://{auth}@zproxy.lum-superproxy.io:9222'
+browser_url = f'https://{auth}@{host}'
 
 async def main():
     async with async_playwright() as pw:
@@ -19,9 +21,11 @@ async def main():
         print('connected')
         page = await browser.new_page()
         print('goto')
-        await page.goto('https://example.com', timeout=120000)
-        # print('done, evaluating')
-        # print(await page.evaluate('()=>document.documentElement.outerHTML'))
+        await page.goto('https://almmello.com', timeout=120000)
+        print('done, evaluating')
+        html_content = await page.evaluate('()=>document.documentElement.outerHTML')
+        with open('output.html', 'w') as f:
+            f.write(html_content)
         await browser.close()
 
 asyncio.run(main())
